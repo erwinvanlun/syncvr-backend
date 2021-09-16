691b3b69d1c7968c5226d048828ec880706fc1eb
Erwin van Lun <erwin@vanlun.com>
2021-09-16 23:37:08 +0200

    Added Access-Control-Allow-Origin' to /history getter

--- src/fibonacci/fibonacci.controller.ts
+++ src/fibonacci/fibonacci.controller.ts
@@ -1,13 +1,19 @@
 import {Controller, Get, Header, Param} from '@nestjs/common';
-import { IpAddress } from 'src/lib/decorators/request-ip';
+import {IpAddress} from 'src/lib/decorators/request-ip';
 import {FibonacciService} from "./fibonacci.service";
-import { RealIP } from 'nestjs-real-ip';
+import {RealIP} from 'nestjs-real-ip';
 
 @Controller('fibonacci')
 export class FibonacciController {
-    constructor(private service: FibonacciService) { }
+    constructor(private service: FibonacciService) {
+    }
+
     @Get('/history') // todo: centralise these url in StackLib
+    @Header('Access-Control-Allow-Origin', '*') // necessary?
+    @Header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT') // necessary?
     getHistory() {
+        console.log('in getHistory!');
+
         return this.service.getHistory();
     }
 
@@ -15,14 +21,14 @@ export class FibonacciController {
     get(@RealIP() ip: string): string {
         // ::1 is the compressed format IPV6 loopback address 0:0:0:0:0:0:0:1.
         // It is the equivalent of the IPV4 address 127.0. 0.1.
-        return ip == '::1'? 'localhost' : ip;
+        return ip == '::1' ? 'localhost' : ip;
     }
 
     @Get(':number')
-    @Header ('Access-Control-Allow-Origin', '*') // necessary?
-    @Header ('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT') // necessary?
+    @Header('Access-Control-Allow-Origin', '*') // necessary?
+    @Header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT') // necessary?
     getNumber(@Param() params, @RealIP() ip: string) { // implicit any, don't like it
-        return this.service.getNumber$(params.number, ip );
+        return this.service.getNumber$(params.number, ip);
     };
 
     @Get()
