0bf1937c829073ee1e31536e6656ae4962a15b95
Erwin van Lun <erwin@vanlun.com>
2021-09-16 22:16:37 +0200

    Hello message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'SyncVR API Server works!';
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'SyncVR API works!';
+    return 'SyncVR API Server works!';
   }
 }
